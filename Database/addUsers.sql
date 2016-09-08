dac1142bffb83276dc78208e4c836f38541471a5
pipposheng <pipposheng@gmail.com>
2016-09-08 09:42:45 -0500

    Scripts updated to include field name to insert query and two conditions for WHERE clause.

--- Database/addUsers.sql
+++ Database/addUsers.sql
@@ -1,23 +1,42 @@
 USE oakpark;
 
-INSERT INTO People VALUES (
-"b980d506-7505-11e6-8735-aaaa55ad65ef", 
+INSERT INTO People
+(PeopleID,
+LastName, FirstName, Gender,
+AddressLine1, AddressLine2,
+City, State, Zipcode,
+Phone, Email, Wechat) 
+VALUES ("b980d506-7505-11e6-8735-aaaa55ad65ef",
 "Sheng", "Biqing", "M", "1193 S Grove Ave", "Unit 3", 
 "Oak Park", "IL", "60304", 
 "8505668553", "pipposheng@gmail.com", "shengbiqing");
 
-INSERT INTO People VALUES (
-"37d43d8a-7506-11e6-8735-aaaa55ad65ef", 
+INSERT INTO People
+(PeopleID,
+LastName, FirstName, Gender,
+AddressLine1, AddressLine2,
+City, State, Zipcode,
+Phone, Email, Wechat)
+VALUES ("37d43d8a-7506-11e6-8735-aaaa55ad65ef",
 "Wang", "Bill", "M", "1139 S Oak Park Ave", "Unit D", 
 "Oak Park", "IL", "60304", 
 "##########", "tianyue901@gmail.com", "tianyue901");
 
-INSERT INTO Driver VALUES (
+INSERT INTO Driver
+(DriverID,
+PeopleID,
+VehicleMaker, VehicleModel, VehicleColor,
+LicensePlate)
+VALUES (
 "4e30a6b8-7506-11e6-8735-aaaa55ad65ef", 
 "b980d506-7505-11e6-8735-aaaa55ad65ef",
 "VW", "Tiguan", "Grey", "V771599");
 
-INSERT INTO Passenger VALUES (
+INSERT INTO Passenger
+(PassengerID,
+PeopleID,
+numberOfLuggage,FlightNumber,ArrivingDate)
+VALUES (
 "80e05860-7506-11e6-8735-aaaa55ad65ef",
 "37d43d8a-7506-11e6-8735-aaaa55ad65ef",
 2, "HU497", "2016-06-23");
--- Database/deleteUsers.sql
+++ Database/deleteUsers.sql
@@ -1,10 +1,10 @@
 USE oakpark;
 
 DELETE FROM People
-WHERE PeopleID="b980d506-7505-11e6-8735-aaaa55ad65ef";
+WHERE PeopleID="b980d506-7505-11e6-8735-aaaa55ad65ef" AND LastName="Sheng";
 
 DELETE FROM Passenger
-WHERE PeopleID="37d43d8a-7506-11e6-8735-aaaa55ad65ef";
+WHERE PeopleID="37d43d8a-7506-11e6-8735-aaaa55ad65ef" AND FlightNumber="HU497";
 
 SELECT * from People;
 SELECT * from Driver;
